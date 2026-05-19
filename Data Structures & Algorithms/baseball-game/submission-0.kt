7d4b63b4de82fc2be4f29ea1e5c797f72cc2aaa4
Matheus Quintas <36519203+quintasmatheus@users.noreply.github.com>
2026-05-19 23:30:30 +0100

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.kt
@@ -0,0 +1,39 @@
+class Solution {
+    fun calPoints(operations: Array<String>): Int {
+        var filteredOperations = mutableListOf<Int>()
+
+        for (op in operations){
+           when(op) {
+            "+" -> {
+                //sum the last two
+                // 1, 2
+                val top = filteredOperations.removeLast()
+                // 1
+                val newTop = top + filteredOperations.last()
+                filteredOperations.add(top)
+                //1, 2
+                filteredOperations.add(newTop)
+                //1,2,3
+
+                // why all this?
+                //in a stack, we only have access to the last item, so to have the
+                // last and the penultimate, we need to remove the top, in order to the
+                //penultimate to become the "newTop", now we have access to both values
+                //we add previous top we removed, back again
+                //and increase the result
+            }
+            "D" -> {
+                val doubleTop = filteredOperations.last() * 2
+                filteredOperations.add(doubleTop)
+            }
+            "C" -> {
+                filteredOperations.removeLast()
+            }
+            else -> {
+                filteredOperations.add(op.toInt())
+            }
+           }
+        }
+       return filteredOperations.sum()
+    }
+}
